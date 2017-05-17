d94ce581642305f4ffaf0c1fc9861ee9142ec9d8
Abraham Velazquez Tello <abraham.velazquez.tello@gmail.com>
2017-05-17 15:16:54 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,5 @@
 /*global jQuery */
 /*jshint browser:true */
-/*!
-* FitVids 1.1
-*
-* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
-* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
-* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
-*
-*/
-
-(function( $ ){
-
-  "use strict";
-
-  $.fn.fitVids = function( options ) {
-    var settings = {
-      customSelector: null
-    };
-
-    if(!document.getElementById('fit-vids-style')) {
-      // appendStyles: https://github.com/toddmotto/fluidvids/blob/master/dist/fluidvids.js
-      var head = document.head || document.getElementsByTagName('head')[0];
-      var css = '.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
-      var div = document.createElement('div');
-      div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + '</style>';
-      head.appendChild(div.childNodes[1]);
-    }
-
-    if ( options ) {
-      $.extend( settings, options );
-    }
-
-    return this.each(function(){
-      var selectors = [
-        "iframe[src*='player.vimeo.com']",
-        "iframe[src*='youtube.com']",
-        "iframe[src*='youtube-nocookie.com']",
-        "iframe[src*='kickstarter.com'][src*='video.html']",
-        "object",
-        "embed"
-      ];
-
-      if (settings.customSelector) {
-        selectors.push(settings.customSelector);
-      }
-
-      var $allVideos = $(this).find(selectors.join(','));
-      $allVideos = $allVideos.not("object object"); // SwfObj conflict patch
-
-      $allVideos.each(function(){
-        var $this = $(this);
-        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
-        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
-            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
-            aspectRatio = height / width;
-        if(!$this.attr('id')){
-          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-          $this.attr('id', videoID);
-        }
-        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
-        $this.removeAttr('height').removeAttr('width');
-      });
-    });
-  };
-// Works with either jQuery or Zepto
-})( window.jQuery || window.Zepto );
-
 /**
  * Main JS file for Casper behaviours
  */
@@ -74,12 +8,7 @@
 (function ($, undefined) {
     "use strict";
 
-    var $document = $(document);
-
-    $document.ready(function () {
-
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+    $(document).ready(function () {
 
         $(".scroll-down").arctic_scroll();
 
@@ -89,21 +18,20 @@
         });
 
         var id = '35581';
-        var token = '35581.71f3d8d.ce32333f83b14fb296a7b61c3a414152'
+        var token = '35581.71f3d8d.cf11bd26407e4217a4267aa7dd2680be'
         var $image = $('.main-header-image');
         $.ajax({
           dataType: "jsonp",
           url: 'https://api.instagram.com/v1/users/' + id + '/media/recent/?access_token=' + token,
+          error: function(response){
+            
+          }
           success: function(response) {
-            console.log(response)
-            var rand = Math.floor(Math.random()*20);
-            var image = response.data[rand].images.low_resolution.url.replace('320x320', '1080x1080');
+            var rand = Math.floor(Math.random() * response.data.length);
+            var image = response.data[rand].images.low_resolution.url.replace('320x320');
             $image.css({'background-image': 'url(' + image +')'}).addClass('loaded');
           }
         });
-
-
-
     });
 
     // Arctic Scroll by Paul Adam Davis
